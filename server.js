3c8aa3143e3bd2f26193bc5a0fa99012c4341a2d
Anthony Gelder <anthony.gelder@gmail.com>
2019-10-11 21:26:44 -0700

    ready for deployment

--- server.js
+++ server.js
@@ -7,48 +7,47 @@ const MOVIES = require('./movies-data-small.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-
-    console.log('validate bearer token middleware')
-
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
-      return res.status(401).json({ error: 'Unauthorized request' })
+        return res.status(401).json({ error: 'Unauthorized request' })
     }
-
-    // move to the next middleware
     next()
 })
 
-function handleGetMovies(req, res) {
-    res.send('Hello, Movies!')
-}
 
-app.get('/movies', function handleGetMovies(req, res) {
+app.get('/movies', (req, res) => {
     let response = MOVIES;
-
     if (req.query.genre) {
         response = MOVIES.filter(movie => movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()))
     }
-
     if (req.query.country) {
         response = MOVIES.filter(movie => movie.country.toLowerCase().includes(req.query.country.toLowerCase()))
     }
-    console.log(req.query.avg_vote)
     if (req.query.avg_vote) {
         response = MOVIES.filter(movie => movie.avg_vote >= req.query.avg_vote)
     }
-
     res.json(response)
 })
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' }}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+    console.log(`Server listening at http://localhost:${PORT}`)
 })
